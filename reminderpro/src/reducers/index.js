17c23027a68b91cbe2069f3afe0e1c573afede1b
Brian W <Briantechdom@gmail.com>
2018-04-08 16:55:35 -0700

    Added sfcookies module.  Updated switch case with sfcookies methods to store and delete cookies.

--- reminderpro/src/reducers/index.js
+++ reminderpro/src/reducers/index.js
@@ -1,6 +1,7 @@
 //Reducers
 
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie} from 'sfcookies';
 
 const reminder = (action) => {
 	let {text, dueDate } = action;
@@ -21,13 +22,16 @@ const removeById = (state = [], id) => {
 const reminders = (state= [], action) => {
 	
 	let reminders = null;
+	state = read_cookie('reminders');
 	switch(action.type) {
 		case ADD_REMINDER: 
 			reminders = [...state, reminder(action)]
-			console.log('reminders as state, reminders');
+			bake_cookie('reminders', reminders);
+			console.log('reminders as state, reminders');			
 			return reminders;
 		case DELETE_REMINDER:
 			reminders = removeById(state, action.id);
+			bake_cookie('reminders', reminders);
 			return reminders;			
 	default:
 		return state;
