d3ce4e05b39b6f401e76a0670a9562923047d317
Brian W <Briantechdom@gmail.com>
2018-04-08 14:19:53 -0700

    Added Redux reducers

--- reminderpro/src/components/App.jsx
+++ reminderpro/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import App form './components/App';
+import '../App.css';
+
 
 
 class App extends Component {
--- reminderpro/src/constants.js
+++ reminderpro/src/constants.js
@@ -1,1 +1,1 @@
-export cons ADD_REMINDER = 'ADD_REMINDER';
+export const ADD_REMINDER = 'ADD_REMINDER';
